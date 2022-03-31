44c222d3c5f3467af6992bc22e6b258cf0102709
Sömen <somendaniel7@gmail.com>
2022-03-31 17:32:15 +0200

    feat: Creating a component which displays one user datas.

---
+++ src/components/User.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function User(props) {
+  return (
+    <div>
+      <h1>{props.firstName}asd</h1>
+      <h1>{props.lastName}asd</h1>
+      <h1>{props.createdAt}asd</h1>
+      <p>-----------------------</p>
+    </div>
+  );
+}
+
+export default User;
