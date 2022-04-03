5eb4706c2c1af5b41fdd896be95bd6c9893a34c5
Sömen <somendaniel7@gmail.com>
2022-04-03 19:22:23 +0200

    feat: Added new route links.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import "./App.css";
+import EditPage from "./components/EditPage";
+import NewUserPage from "./components/NewUserPage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
+        <Route path="/edit/:id" element={<EditPage />}></Route>
+        <Route path="/new" element={<NewUserPage />}></Route>
       </Routes>
     </Router>
   );
