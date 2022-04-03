116690b47747da48b1de8fd067e9ce74072f2a02
Sömen <somendaniel7@gmail.com>
2022-04-03 19:27:08 +0200

    feat: Creating status update functionality.

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,67 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function User(props) {
+  let userData = props.data;
+
+  // need a page refresh after status change.
+  const changeStatus = () => {
+    if (props.data.status === "active") {
+      fetch(
+        `https://assessment-users-backend.herokuapp.com/users/${userData.id}`,
+        {
+          method: "put",
+          body: JSON.stringify({
+            created_at: userData.createdAt,
+            first_name: userData.first_name,
+            id: userData.id,
+            last_name: userData.last_name,
+            status: "locked",
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    } else {
+      fetch(
+        `https://assessment-users-backend.herokuapp.com/users/${userData.id}`,
+        {
+          method: "put",
+          body: JSON.stringify({
+            created_at: userData.createdAt,
+            first_name: userData.first_name,
+            id: userData.id,
+            last_name: userData.last_name,
+            status: "active",
+          }),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
+  console.log(userData.status);
   return (
     <div>
-      <h1>{props.firstName}asd</h1>
-      <h1>{props.lastName}asd</h1>
-      <h1>{props.createdAt}asd</h1>
+      <h1>{props.firstName}</h1>
+      <h1>{props.lastName}</h1>
+      <h1>{props.createdAt}</h1>
+      <h1>{userData.status}</h1>
       <p>-----------------------</p>
+      <button onClick={changeStatus}>check</button>
+      <button>
+        <Link to={`/edit/${props.data.id}`}>Edit</Link>
+      </button>
     </div>
   );
 }
